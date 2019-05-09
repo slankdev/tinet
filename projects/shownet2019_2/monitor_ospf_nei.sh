2a62629ac0ffc8d43c49ace8bc1ac3273dfe7809
Hiroki Shirokura <slankdev@nttv6.jp>
2019-05-09 16:11:08 +0000

    2019.05.09-16:11

--- projects/shownet2019_2/monitor_ospf_nei.sh
+++
@@ -1,6 +0,0 @@
-
-docker exec H1 birdc6 -v show ospf nei ospf_func1_up
-docker exec H1 birdc6 -v show ospf nei ospf_func1_dn
-docker exec H1 birdc6 -v show ospf nei ospf_func2_up
-docker exec H1 birdc6 -v show ospf nei ospf_func2_dn
-
