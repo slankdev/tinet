45f0522cd2b2819a5e40cb0cb341d6795db67ca8
Hiroki Shirokura <slankdev@nttv6.jp>
2019-05-09 14:35:52 +0000

    2019.05.09-14:35

--- projects/shownet2019_2/monitor_ospf_nei.sh
+++ projects/shownet2019_2/monitor_ospf_nei.sh
@@ -1,20 +1,6 @@
 
-echo -n 'R1 '
-docker exec R1 vtysh -c 'show ipv6 ospf6 nei'
-
-echo
-echo -n 'R2 '
-docker exec R2 vtysh -c 'show ipv6 ospf6 nei'
-
-echo
-echo -n 'R3 '
-docker exec R3 vtysh -c 'show ipv6 ospf6 nei'
-
-echo
-echo -n 'R4 '
-docker exec R4 vtysh -c 'show ipv6 ospf6 nei'
-
-echo
-echo -n 'F1 '
-docker exec F1 vtysh -c 'show ipv6 ospf6 nei'
+docker exec H1 birdc6 -v show ospf nei ospf_func1_up
+docker exec H1 birdc6 -v show ospf nei ospf_func1_dn
+docker exec H1 birdc6 -v show ospf nei ospf_func2_up
+docker exec H1 birdc6 -v show ospf nei ospf_func2_dn
 
