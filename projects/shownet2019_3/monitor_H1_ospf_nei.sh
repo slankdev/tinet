e0603282d275f3d1ced7f6e2a5283a02c4e6339a
Hiroki Shirokura <slankdev@nttv6.jp>
2019-05-12 06:17:33 +0000

    2019.05.12-06:17

--- projects/shownet2019_3/monitor_H1_ospf_nei.sh
+++ projects/shownet2019_3/monitor_H1_ospf_nei.sh
@@ -4,16 +4,22 @@ docker exec H1 birdc6 -v show ospf nei ospf_func1_up | grep -v "0001 BIRD 1.6.6 
 docker exec H1 birdc6 -v show ospf nei ospf_func1_dn | grep -v "0001 BIRD 1.6.6 ready"
 docker exec H1 birdc6 -v show ospf nei ospf_func2_up | grep -v "0001 BIRD 1.6.6 ready"
 docker exec H1 birdc6 -v show ospf nei ospf_func2_dn | grep -v "0001 BIRD 1.6.6 ready"
-docker exec H1 birdc6 -v show ospf nei ospf_func3_up | grep -v "0001 BIRD 1.6.6 ready"
-docker exec H1 birdc6 -v show ospf nei ospf_func3_dn | grep -v "0001 BIRD 1.6.6 ready"
-docker exec H1 birdc6 -v show ospf nei ospf_func4_up | grep -v "0001 BIRD 1.6.6 ready"
-docker exec H1 birdc6 -v show ospf nei ospf_func4_dn | grep -v "0001 BIRD 1.6.6 ready"
+#docker exec H1 birdc6 -v show ospf nei ospf_func3_up | grep -v "0001 BIRD 1.6.6 ready"
+#docker exec H1 birdc6 -v show ospf nei ospf_func3_dn | grep -v "0001 BIRD 1.6.6 ready"
+#docker exec H1 birdc6 -v show ospf nei ospf_func4_up | grep -v "0001 BIRD 1.6.6 ready"
+#docker exec H1 birdc6 -v show ospf nei ospf_func4_dn | grep -v "0001 BIRD 1.6.6 ready"
+
+echo "\n# H1 inet #"
+docker exec H1 ip -6 route list vrf inet
+
+echo "\n# H1 user #"
+docker exec H1 ip -6 route list vrf user
 
 echo "\n# H1 func1_up #"
 docker exec H1 ip -6 route list vrf func1_up
 
 echo "\n# H1 func1_dn #"
-docker exec H1 ip -6 route list vrf func1_up
+docker exec H1 ip -6 route list vrf func1_dn
 
 echo "\n# F1 route #"
 docker exec F1 ip -6 route list
